cfa4f79490be06fe5f5adcc1e776b7d03f90bcce
pwrwpw <pwrwpw.dev@gmail.com>
2025-05-13 18:15:01 +0900

    프로젝트 초기 설정 및 MSA 기반 서비스 구조 구성

---
+++ BE/common/src/test/java/kr/rssreader/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.rssreader.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ BE/core/src/test/java/kr/rssreader/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.rssreader.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
